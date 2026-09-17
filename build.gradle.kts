6f27905efd6689d6067a7270b1d4b9e4d4153ff9
Weitspringer <luca.springer@posteo.de>
2026-09-17 17:09:46 +0200

    RISDEV-12442 Update default library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 group = "de.bund.digitalservice.ris"
-version = System.getenv("RELEASE_VERSION") ?: "0.7.5"
+version = System.getenv("RELEASE_VERSION") ?: "0.0.1"
 
 java {
   toolchain {
